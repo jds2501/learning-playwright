cd5a76bc636df46605b6dcec6c01a6530067d99f
Jason Smith <jason@descript.com>
2024-01-08 16:02:05 -0800

    Mapped steps to requested testing problem

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -12,28 +12,28 @@ test('Register Account Interactions', async ({page})=>
 
 test('Login Test', async ({page})=>
 {
-    //Login
+    // 1. Login
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("descript.linking@gmail.com");
     await page.locator("#userPassword").fill("Lindy123$");
     await page.locator("#login").click();
 
-    //Check first item in list is there post login
+    //Check last item in list is there post login
     const allTitleContents = await page.locator(".card-body b");
     await allTitleContents.last().waitFor();
-    let targetProduct = undefined;
 
+    // 2. Print titles on page
+    const allTitles = await allTitleContents.allTextContents();
+    console.log(allTitles);
+
+    // 3. Find the specific product to buy
+    const desiredProductName = "ADIDAS ORIGINAL"
+    let targetProduct = undefined;
     for (let i = 0; i < await allTitleContents.count() && targetProduct == undefined; i++){
-        if (await allTitleContents.nth(i).textContent() == "ADIDAS ORIGINAL"){
+        if (await allTitleContents.nth(i).textContent() == desiredProductName){
             targetProduct = allTitleContents.nth(i);
         }
     }
-
-    console.log(await targetProduct.textContent());
-
-    const allTitles = await allTitleContents.allTextContents();
-    console.log(allTitles);
-    await expect(page.locator("[style*='uppercase']").first()).toContainText('ZARA');
-
+    await expect(await targetProduct != undefined).toBeTruthy();
 
 });
