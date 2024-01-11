f431ed58a32d264f83824a060bd131be2678c41c
Jason Smith <jasondsmith2501@gmail.com>
2024-01-10 22:48:56 -0800

    Finished basic playwright exercise

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -10,7 +10,7 @@ test('Register Account Interactions', async ({page})=>
 });
 
 
-test('Login Test', async ({page})=>
+test('Playwright Practice Exercise', async ({page})=>
 {
     // Login
     const email = "descript.linking@gmail.com";
@@ -80,4 +80,22 @@ test('Login Test', async ({page})=>
     const orderHistoryIDIndex = orderHistoryIDs.indexOf(orderID[0]);
     expect(orderHistoryIDIndex >= 0).toBeTruthy();
     await orderHistoryRows.nth(orderHistoryIDIndex).locator(".btn.btn-primary").click();
+
+    // Check order ID, product name, and billing / delivery address titles on order summary page
+    await expect(page.locator(".col-text.-main")).toHaveText(orderID[0]);
+    await expect(page.locator(".title")).toHaveText(productName);
+    const orderSummaryAddresses = page.locator(".address");
+    expect(await orderSummaryAddresses.count()).toEqual(2);
+    await expect(orderSummaryAddresses.nth(0).locator(".content-title")).toContainText("Billing Address");
+    await expect(orderSummaryAddresses.nth(1).locator(".content-title")).toContainText("Delivery Address");
+
+    // Check that email & countries are correct on order summary page
+    for (let i = 0; i < 2; i++){
+        const emailCountry = orderSummaryAddresses.nth(i).locator(".text");
+        expect(await emailCountry.count()).toEqual(2);
+        await expect(emailCountry.nth(0)).toContainText(email);
+        await expect(emailCountry.nth(1)).toContainText("United States");
+    }
+
+
 });
