14f072953e04f42e6e745a34053566edac50b181
Jason Smith <jasondsmith2501@gmail.com>
2024-01-12 11:01:17 -0800

    Added more special locators

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -6,4 +6,9 @@ test('Playwright Special locators', async ({page})=>
     await page.getByLabel("Check me out if you Love IceCreams!").click();
     await page.getByLabel("Employed").check();
     await page.getByLabel("Gender").selectOption("Female");
+    await page.getByPlaceholder("Password").fill("test");
+    await page.getByRole("button", {name: "Submit"}).click();
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link", {name: "Shop"}).click();
+    await page.locator("app-card").filter({hasText: "Nokia Edge"}).getByRole("button").click();
 });
