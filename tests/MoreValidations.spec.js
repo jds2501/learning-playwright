f75a912c9be70e52943bbc3b941c857b349f4521
Jason Smith <jasondsmith2501@gmail.com>
2024-01-12 15:46:43 -0800

    Added more validations

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,17 @@
+const {test, expect} = require('@playwright/test');
+
+test('Popup validations', async ({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    //await page.goto("http://google.com");
+    //await page.goBack();
+    //await page.goForward();
+    const displayedText = page.locator("#displayed-text");
+    await expect(displayedText).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(displayedText).toBeHidden();
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator("#confirmbtn").click();
+    await page.locator("#mousehover").hover();
+    await page.pause();
+});
