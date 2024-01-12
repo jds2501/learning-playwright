b80e02843dff9b74a96b8a63041705325d2c1955
Jason Smith <jasondsmith2501@gmail.com>
2024-01-11 20:56:58 -0800

    Added get by label

---
+++ tests/llc.spec.js
@@ -0,0 +1,9 @@
+const {test, expect} = require('@playwright/test');
+
+test('Playwright Special locators', async ({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
+});
