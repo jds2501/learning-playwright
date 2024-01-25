788ceee628a385f7075d2e568e41ba5eb0d23192
Jason Smith <jasondsmith2501@gmail.com>
2024-01-25 10:53:05 -0800

    Added custom config file

---
+++ playwright.config1.js
@@ -0,0 +1,38 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  projects: [
+    {
+      name: 'Safari',
+      use: {
+        browserName: 'webkit',
+        headless: false,
+        screenshot : 'only-on-failure',
+        trace: 'retain-on-failure'
+      }
+    },
+    {
+      name : 'Chrome',
+      use: {
+        browserName: 'chromium',
+        headless: true,
+        screenshot : 'only-on-failure',
+        trace: 'retain-on-failure'
+      }
+    }
+
+  ]
+});
